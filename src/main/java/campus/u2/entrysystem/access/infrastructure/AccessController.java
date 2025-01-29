d6acdc861ec842a4089428121ccd658c9eec32d6
JaimePrada11 <jandrespradato.11@gmail.com>
2025-01-29 05:30:43 -0500

    Actualizacion del acceso controller

--- src/main/java/campus/u2/entrysystem/access/infrastructure/AccessController.java
+++ src/main/java/campus/u2/entrysystem/access/infrastructure/AccessController.java
@@ -3,11 +3,15 @@ package campus.u2.entrysystem.access.infrastructure;
 import campus.u2.entrysystem.access.application.AccessService;
 import campus.u2.entrysystem.access.domain.Access;
 import campus.u2.entrysystem.accessnotes.domain.AccessNote;
+import campus.u2.entrysystem.people.application.PeopleService;
+import campus.u2.entrysystem.people.domain.People;
 import campus.u2.entrysystem.porters.application.PortersService;
 import campus.u2.entrysystem.porters.domain.Porters;
 import java.util.Date;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,15 +26,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/access")
 public class AccessController {
 
-    // Attributes 
+    // Attributes  
     private final AccessService accessService;
     private final PortersService porterService;
+    private final PeopleService peopleService;
 
     // Constructor 
     @Autowired
-    public AccessController(AccessService accessService, PortersService porterService) {
+    public AccessController(PeopleService peopleService, AccessService accessService, PortersService porterService) {
         this.accessService = accessService;
         this.porterService = porterService;
+        this.peopleService = peopleService;
     }
 
     // Methods 
@@ -47,9 +53,18 @@ public class AccessController {
     }
 
     // To create a new access 
-    @PostMapping
-    public Access createAccess(@RequestBody Access acccess) {
-        return accessService.saveAccess(acccess);
+    @PostMapping("/people/{idpeople}")
+    public ResponseEntity<Access> createAccessWithPersonAndPorter(@PathVariable String idpeople,
+            @RequestBody Access acccess) {
+        People people = peopleService.getPeopleById(idpeople);
+        if (people != null) {
+
+            acccess.setPeople(people);
+            Access savedAccess = accessService.saveAccess(acccess);
+
+            return new ResponseEntity<>(savedAccess, HttpStatus.CREATED);
+        }
+        return null;
     }
 
     // To delete an access 
@@ -63,11 +78,11 @@ public class AccessController {
     public Access addAccessNoteToAccess(@PathVariable String idAccess, @RequestBody AccessNote accessNote) {
         return accessService.addAccessNoteToAccess(idAccess, accessNote);
     }
-    
+
     //To remove a note to an access 
     @DeleteMapping("/delete-note/{idAccessNote}")
     public Access deleteAccessNoteToAccess(@PathVariable String idAccessNote) {
-        return accessService.removeAccessNoteFromAccess(idAccessNote); 
+        return accessService.removeAccessNoteFromAccess(idAccessNote);
     }
 
     // To add a porter to an access
@@ -93,7 +108,27 @@ public class AccessController {
     public Access updatePorterInAccess(@PathVariable String idAccess, @PathVariable String idPorter) {
         Access access = accessService.getAccessById(idAccess);
         Porters porter = porterService.getPorterById(idPorter);
-        access.getPorters().add(porter); 
+        access.getPorters().add(porter);
         return accessService.saveAccess(access);
     }
+
+    @PutMapping("/{idAccess}")
+    public Access updateAccess(@PathVariable String idAccess, @RequestBody Access updatedAccess) {
+        Access access = accessService.getAccessById(idAccess);
+
+        if (access != null) {
+
+            if (updatedAccess.getEntryAccess() != null) {
+                access.setEntryAccess(updatedAccess.getEntryAccess());
+            }
+            if (updatedAccess.getExitAccess() != null) {
+                access.setExitAccess(updatedAccess.getExitAccess());
+            }
+            return accessService.saveAccess(access);
+        }
+
+        return null;
+
+    }
+
 }
